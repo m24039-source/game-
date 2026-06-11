c07304c3b8c3f1f6290e0c50fd392b5da495e9f0
m24039-source <m24039@g.metro-cit.ac.jp>
2026-06-12 01:11:01 +0900

    Update cpu-ai.js

--- cpu-ai.js
+++ cpu-ai.js
@@ -13,7 +13,7 @@ let aiNeuralModel = null;
 export async function loadBrain() {
     try {
         // 同じフォルダ階層にある tfjs_model/model.json をロード
-        aiNeuralModel = await tf.loadLayersModel('tfjs_model/model.json');
+        aiNeuralModel = await tf.loadLayersModel('/game-/tfjs_model/model.json');
         
         console.log("🧠 [AI] Google Colabで鍛え上げた最強の脳みそを正常にロードしました！");
         return true;
